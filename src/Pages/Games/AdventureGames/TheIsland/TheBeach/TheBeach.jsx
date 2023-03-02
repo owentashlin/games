1f4971bb75d5056613014e2720109a2eac2b602e
owentashlin <owentashlin@gmail.com>
2023-03-01 16:27:37 -0800

    add image to island path

--- src/Pages/Games/AdventureGames/TheIsland/TheBeach/TheBeach.jsx
+++ src/Pages/Games/AdventureGames/TheIsland/TheBeach/TheBeach.jsx
@@ -1,51 +1,91 @@
 import React from "react";
-import {useState} from 'react'
-import LightFire from "./LightFire"
-import FindWater from "./FindWater"
-import {GiCampfire, GiShipWreck, GiWaterFlask, GiShrug} from "react-icons/gi";
-import image from '../../images/beach3.png'
-
+import { useState } from "react";
+import LightFire from "./LightFire";
+import FindWater from "./FindWater";
+import { GiCampfire, GiShipWreck, GiWaterFlask, GiShrug } from "react-icons/gi";
+import image from "../../images/beach3.png";
 
 function TheBeach() {
+  const [toggleFire, setToggleFire] = useState(false);
+  const [toggleWater, setToggleWater] = useState(false);
 
-const [toggleFire, setToggleFire] = useState(false)
-const [toggleWater, setToggleWater] = useState(false)
+  const handleToggleFire = () => {
+    setToggleFire(!toggleFire);
+    setToggleWater(false);
+    document.getElementById("water").disabled = true;
+  };
 
-const handleToggleFire = () => {
-    setToggleFire(!toggleFire)
-    setToggleWater(false)
-    document.getElementById("water").disabled = true
-}
+  const handleToggleWater = () => {
+    setToggleWater(!toggleWater);
+    setToggleFire(false);
+    document.getElementById("campfire").disabled = true;
+  };
 
-const handleToggleWater = () => {
-    setToggleWater(!toggleWater)
-    setToggleFire(false)
-    document.getElementById("campfire").disabled = true
-}
+  return (
+    <div
+      className="games"
+      style={{ backgroundImage: `url(${image})`, backgroundSize: "cover" }}
+    >
+      <div>
+        <div style={{ background: "white", opacity: "80%" }}>
+          <p>
+            <GiShipWreck size="4rem" />
+          </p>
+          <h3>The Beach</h3>
+          <div style={{ margin: "5px", padding: "5px" }}>
+            <p>
+              You wake up with the feeling of warm sand beneath you. You can feel the sun beating down on your back, and gentle waves lap at your feet and legs. You open your eyes and blink, raising your head to look around. You see you are on a beach, and as you look around you can see the wreckage of a ship washed ashore all around you. You remember then, the storm,the ship going down with you still on board, the desperate swim for what looked like land, the flash of lightning and the waves, then black.
+            </p>
+            <p>
+              You lick your lips as you lever yourself upright, trying to get your bearings.
+            </p>
+            <p>
+              You need water, and as you shade your eyes from the sun, you realize you need shade as well. Looking farther up the beach away
+              from the sea, you can see a dense line of jungle and what looks
+              like it might be a path leading deeper in.
+            </p>
 
-    return ( 
-        <div className='games' style={{ backgroundImage:`url(${image})`,backgroundSize:"cover"}}>
-        <div>
-        <div style={{background:'white', opacity:'80%'}}>
-        <p><GiShipWreck size='4rem'/></p>
-        <h3>The Beach</h3>
-        <div style={{margin:'5px', padding:'5px'}}>
-        <p>You wake up with the feeling of warm sand beneath you. You can feel the sun beating down on your back, and gentle waves lap at your feet and legs. You open your eyes and blink, raising your head to look around. You see you are on a beach, and as you look around you can see the wreckage of a ship washed ashore all around you. You remember then, the storm,the ship going down with you still on board, the desperate swim for what looked like land, the flash of lightning and the waves, then black.</p>
-        <p>You lick your lips as you lever yourself upright, trying to get your bearings.</p>
-        <p>You need water, and as you shade your eyes from the sun, you realize you need shade as well. Looking farther up the beach away from the sea, you can see a dense line of jungle and what looks like it might be a path leading deeper in.</p>
-
-        <p>Looking back out to sea, you wonder if anyone is coming to rescue you. Should you wait here and try to light a signal fire or head into the jungle and try to find water? You can't forget either, you came here on a mission, find the treasure!</p>
+            <p>
+              Looking back out to sea, you wonder if anyone is coming to rescue
+              you. Should you wait here and try to light a signal fire or head
+              into the jungle and try to find water? You can't forget either,
+              you came here on a mission, find the treasure!
+            </p>
+          </div>
         </div>
+        <div
+          style={{
+            background: "white",
+            opacity: "80%",
+            margin: "20px",
+            paddingTop: "1px",
+            paddingBottom: "15px",
+            paddingLeft: "2px",
+            paddingRight: "2px",
+          }}
+        >
+          <h3>Light a fire and wait for rescue or go look for water?</h3>
+          {toggleWater === false ? (
+            <button id="water">
+              <GiWaterFlask onClick={handleToggleWater} size="4rem" />
+            </button>
+          ) : (
+            <FindWater />
+          )}
+          <p>
+            <GiShrug size="4rem" />
+          </p>
+          {toggleFire === false ? (
+            <button id="campfire">
+              <GiCampfire onClick={handleToggleFire} size="4rem" />
+            </button>
+          ) : (
+            <LightFire />
+          )}
         </div>
-            <div style={{background:'white', opacity:'80%', margin:'20px', paddingTop:'1px', paddingBottom:'10px'}}>
-        <h3>Light a fire and wait for rescue or go look for water?</h3>
-            {(toggleWater === false)?<button id='water'><GiWaterFlask onClick={handleToggleWater} size='4rem'/></button>:<FindWater/>}
-             <p><GiShrug size='4rem'/></p>
-            {(toggleFire === false)?<button id='campfire'><GiCampfire onClick={handleToggleFire} size='4rem'/></button>:<LightFire/>}
-            </div> 
-        </div>
-        </div>
-     );
+      </div>
+    </div>
+  );
 }
 
 export default TheBeach;
--- src/Pages/Games/AdventureGames/TheIsland/ThePath/ThePath.jsx
+++ src/Pages/Games/AdventureGames/TheIsland/ThePath/ThePath.jsx
@@ -1,43 +1,82 @@
-import { useState } from 'react'
-import TurnLeft from './TurnLeft'
-import TurnRight from './TurnRight'
-import {WiWindDeg} from 'react-icons/wi'
-import {GiShrug, GiForest} from "react-icons/gi"
+import { useState } from "react";
+import TurnLeft from "./TurnLeft";
+import TurnRight from "./TurnRight";
+import { WiWindDeg } from "react-icons/wi";
+import { GiShrug, GiForest } from "react-icons/gi";
+import image from "../../images/path.png";
 
 function ThePath() {
+  const [toggleLeft, setToggleLeft] = useState(false);
+  const [toggleRight, setToggleRight] = useState(false);
 
-const [toggleLeft, setToggleLeft] = useState(false)
-const [toggleRight, setToggleRight] = useState(false)
+  const handleToggleLeft = function () {
+    setToggleLeft(!toggleLeft);
+    setToggleRight(false);
+    document.getElementById("right").disabled = true;
+  };
 
-const handleToggleLeft = function() {
-    setToggleLeft(!toggleLeft)
-    setToggleRight(false)
-    document.getElementById("right").disabled = true
-}
+  const handleToggleRight = function () {
+    setToggleRight(!toggleRight);
+    setToggleLeft(false);
+    document.getElementById("left").disabled = true;
+  };
 
-const handleToggleRight = function() {
-    setToggleRight(!toggleRight)
-    setToggleLeft(false)
-    document.getElementById("left").disabled = true
-}
+  return (
+    <div>
+      <h1>The Mysterious Island</h1>
 
-    return ( 
-        <>
-        <h1>The Mysterious Island</h1>
-        <div className='games'>
-        <h3>The Path</h3>
-        <p><GiForest size='4rem' /></p>
-        <p>Refreshed you stand up again, stepping over the small stream and follow the path father into the forest, until you come to a fork. You can turn left or right. Looking down both paths, they seem to be largely the same. No help for it though, you'll have to pick a direction...</p>
-
-        <h3>Turn left or right?</h3>
-            <div className='games'>
-            {(toggleRight === false)?<button id='right'><WiWindDeg onClick={handleToggleRight} size="4rem" style = {{transform:'rotate(90deg)'}}/></button>:<TurnRight/>}
-            <p><GiShrug size='4rem'/></p>
-            {(toggleLeft === false)?<button id='left'><WiWindDeg onClick={handleToggleLeft} size="4rem" style = {{transform:'rotate(270deg)'}}/></button>:<TurnLeft/>}
-            </div>
+      <div
+        className="games"
+        style={{ backgroundImage: `url(${image})`, backgroundSize: "cover" }}
+      >
+        <div style={{ background: "white", opacity: "80%", padding: '5px' }}>
+          <p>
+            <GiForest size="4rem" />
+          </p>
+          <h3>The Path</h3>
+          <div style={{ margin: "5px", padding: "5px" }}>
+            <p>
+              Refreshed you stand up again, stepping over the small stream and
+              follow the path father into the forest, until you come to a fork.
+              You can turn left or right. Looking down both paths, they seem to
+              be largely the same. No help for it though, you'll have to pick a
+              direction...
+            </p>
+          </div>
         </div>
-        </>
-     );
+        <h3>Turn left or right?</h3>
+        <div className="games" style={{ background: "white", opacity: "80%" }}>
+          {toggleRight === false ? (
+            <button id="right">
+              <WiWindDeg
+                onClick={handleToggleRight}
+                size="4rem"
+                style={{ transform: "rotate(90deg)" }}
+              />
+            </button>
+          ) : (
+            <TurnRight />
+          )}
+
+          <p>
+            <GiShrug size="4rem" />
+          </p>
+
+          {toggleLeft === false ? (
+            <button id="left">
+              <WiWindDeg
+                onClick={handleToggleLeft}
+                size="4rem"
+                style={{ transform: "rotate(270deg)" }}
+              />
+            </button>
+          ) : (
+            <TurnLeft />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ThePath;
