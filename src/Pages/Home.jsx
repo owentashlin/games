8f5b5d0f591b06eb06a071086ef6a6e9e86b0e85
owentashlin <owentashlin@gmail.com>
2023-03-05 13:41:09 -0800

    background image home/games update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,9 +4,9 @@ function Home() {
     return(
         <div>
             <h1>Welcome to Owen's Workshop!</h1>
-            <p className='home-text'>Here you will find a fun group of silly little games that took up way to much brain space while they were being crafted.</p>
-            <p>Hope you like them!</p>
-            <p className='home-image'><GiAnvilImpact size='40rem' color='#f5f5f5'/></p>
+            <p className='home-text'>Here you will find a fun group of silly little games that took up way to much brain space while they were being crafted. Hope you like them!
+            <div className='home-image'><GiAnvilImpact size='50rem' color='#f5f5f5'/></div>
+            </p>
         </div>
     )
 }
