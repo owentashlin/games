d5e7fa1ae5c809de2de2d2c67d9a5622cdf13a7e
owentashlin <owentashlin@gmail.com>
2023-03-06 21:18:37 -0800

    add game blurb

--- src/Pages/Games/Games.jsx
+++ src/Pages/Games/Games.jsx
@@ -9,7 +9,8 @@ function Games() {
         <h1>Owen's Games</h1>
         <div className='home-image'><GiAnvilImpact size='50rem' color='#f5f5f5'/></div>
         <h3><strong>Text Adventures</strong></h3>
-        <Link to='/games/island'>The Mysterious Island</Link>
+        <Link className='game-link' to='/games/island'>The Mysterious Island</Link>
+        <p className='game-blurb'>You wake up on the shore of a tropical island, your ship having gone down in a storm. You have a choice, stay and wait for rescue or go exploring and maybe find the treasure rumored to be hidden somewhere on the island. What will you do?</p>
 
         <h3><strong>Arcade Style</strong></h3>
         <a href='https://owentashlin.github.io/GA--project-1-Simon/'>Simon</a>
