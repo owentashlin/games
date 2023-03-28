7812de8e4abbfa13e71a9438238d3f0c4e4e54b1
owentashlin <owentashlin@gmail.com>
2023-03-28 12:19:17 -0700

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,8 @@ function Home() {
             <p className='home-text'>Here you will find a fun group of silly little games that took up way to much brain space while they were being crafted. Hope you like them!
             </p>
             <br></br>
-
+            <img src='https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=BrownDark&facialHairType=BeardLight&facialHairColor=BrownDark&clotheType=BlazerSweater&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light'
+            alt='avatar'/>
             <hr></hr>
         </div>
     )
